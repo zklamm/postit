27eb6448bf4ef3ccf9805dac3183fa04082f550c
Zac Klammer <zklamm@gmail.com>
2020-04-20 11:39:20 -0500

    Added Category model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200420143431) do
+ActiveRecord::Schema.define(version: 20200420163532) do
+
+  create_table "categories", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "comments", force: true do |t|
     t.text     "body"
