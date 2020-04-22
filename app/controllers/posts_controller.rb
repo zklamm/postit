faa42a188f09c50dd07446144085ab63ee1d6655
Zac Klammer <zklamm@gmail.com>
2020-04-22 11:45:06 -0500

    Completed Post Controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,25 +1,46 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update]
+
   def index
     @posts = Post.all
   end
 
-  def show
-    @post = Post.find(params[:id])
-  end
+  def show; end
 
   def new
-    
+    @post = Post.new
   end
 
   def create
-    
+    @post = Post.new(post_params)
+    @post.creator = User.first # TODO: change once we have authentication
+
+    if @post.save
+      flash[:notice] = "Your post was created."
+      redirect_to posts_path
+    else
+      render :new
+    end
   end
 
-  def edit
-    
-  end
+  def edit; end
 
   def update
-    
+    if @post.update(post_params)
+      flash[:notice] = "This post was updated."
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :url, :description)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,8 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :post_categories
   has_many :categories, through: :post_categories
+
+  validates :title, presence: true, length: {minimum: 5}
+  validates :description, presence: true
+  validates :url, presence: true, uniqueness: true
 end
