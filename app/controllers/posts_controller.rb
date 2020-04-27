a47e2d9e078240fed27a8d9ebb5ea54441c59507
Zac Klammer <zklamm@gmail.com>
2020-04-27 09:39:13 -0500

    Added Authentication

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update]
+  before_action :require_user, except: [:show, :index]
 
   def index
     @posts = Post.all
@@ -15,7 +16,7 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.new(post_params)
-    @post.creator = User.first # TODO: change once we have authentication
+    @post.creator = current_user
 
     if @post.save
       flash[:notice] = "Your post was created."
